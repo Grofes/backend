24cc9205f2ee8729cfc3bd1515c4c5b371ee2be5
Grofes <92209055+Grofes@users.noreply.github.com>
2023-03-26 21:08:27 +0300

    Update form.php

--- num3/form.php
+++ num3/form.php
@@ -4,11 +4,11 @@
 <body>
   <div class="fr1">
   <form action="index.php" method="POST">
-    <label> ФИО </label> <br>
-    <input name="name" /> <br>
-    <label> Почта </label> <br>
-    <input name="email" type="email" /> <br>
-    <label> Год рождения </label> <br>
+    <label> ФИО </label> <br><br>
+    <input name="name" /> <br><br>
+    <label> Почта </label> <br><br>
+    <input name="email" type="email" /> <br><br>
+    <label> Год рождения </label> <br><br>
     <select name="year">
       <option value="Выбрать">Выбрать</option>
     <?php
@@ -16,28 +16,28 @@
           printf("<option value=%d>%d год</option>",$i,$i);
         }
     ?>
-    </select> <br>
-    <label> Ваш пол </label> <br>
+    </select> <br><br>
+    <label> Ваш пол </label> <br><br>
     <div>
       <input name="sex" type="radio" value="1" /> Мужчина
       <input name="sex" type="radio" value="2" /> Женщина
     </div>
-    <label> Сколько у вас конечностей </label> <br>
+    <label> Сколько у вас конечностей </label> <br><br>
     <div>
       <input name="limb" type="radio" value="1" /> 1 
       <input name="limb" type="radio" value="2" /> 2 
       <input name="limb" type="radio" value="3" /> 3 
       <input name="limb" type="radio" value="4" /> 4 
     </div>
-    <label> Выберите суперспособности </label> <br>
+    <label> Выберите суперспособности </label> <br><br>
     <select name="form1[]" size="3" multiple>
       <option value="1">Телпортация</option>
       <option value="2">Бессмертие</option>
       <option value="3">Телепатия</option>
-    </select> <br>
-    <label> Биография </label> <br>
-    <textarea name="bio" rows="10" cols="15"></textarea> <br>
-    <input name="checked" type="checkbox" value="on"> Вы согласны с пользовательским соглашением <br>
+    </select> <br><br>
+    <label> Биография </label> <br><br>
+    <textarea name="bio" rows="10" cols="15"></textarea> <br><br>
+    <input name="checked" type="checkbox" value="on"> Вы согласны с пользовательским соглашением <br><br>
     <input type="submit" value="Отправить"/>
   </form>
   </div>
