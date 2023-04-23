5c5e5090babfaf28c4503954bbd8a6fe3585f06d
Grofes <92209055+Grofes@users.noreply.github.com>
2023-04-23 16:33:15 +0300

    Update form.php

--- num4/form.php
+++ num4/form.php
@@ -53,9 +53,9 @@ if (!empty($messages)) {
     </div>
     <label> Выберите суперспособности </label> <br>
     <select name="form1[]" size="3" multiple <?php if ($errors['form1']) {print 'class="error"';} ?>>
-      <option value="1" <?php if($values['teleport']==1){print 'selected';} ?>>Телепортация</option>
-      <option value="2" <?php if($values['immortal']==1){print 'selected';} ?>>Бессмертие</option>
-      <option value="3" <?php if($values['telepat']==1){print 'selected';} ?>>Телепатия</option>
+      <option value="1" <?php if($values['1']==1){print 'selected';} ?>>Телепортация</option>
+      <option value="2" <?php if($values['2']==1){print 'selected';} ?>>Бессмертие</option>
+      <option value="3" <?php if($values['3']==1){print 'selected';} ?>>Телепатия</option>
     </select> <br>
     <label> Краткая биография </label> <br>
     <textarea name="bio" rows="10" cols="15"><?php print $values['bio']; ?></textarea> <br>
