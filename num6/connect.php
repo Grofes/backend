7eabe595c9c6961ff273e322e8d1dc165125e5ad
Grofes <92209055+Grofes@users.noreply.github.com>
2023-05-02 13:53:53 +0300

    Update connect.php

--- num6/connect.php
+++ num6/connect.php
@@ -1,4 +1,4 @@
 <?php
-$user = 'u47593';
-$pass = '7863466';
-$db = new PDO('mysql:host=localhost;dbname=u47593', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+$user = 'u52821';
+$pass = '8567731';
+$db = new PDO('mysql:host=localhost;dbname=u52821', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
