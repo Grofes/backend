2fc88ecadeb7009f732aa52a3619ec54a2affb8a
Grofes <92209055+Grofes@users.noreply.github.com>
2023-05-23 17:14:16 +0300

    Update login.php

--- num5/login.php
+++ num5/login.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 <div class="form-sign-in">
 <form action="login.php" method="post">
   print("pizda");
-  <label> Логин <label> <br>
+  <label> Логин xyi <label> <br>
   <input name="login" /> <br> 
   <label> Пароль <label> <br>
   <input name="password" type="password"/> <br><br>
