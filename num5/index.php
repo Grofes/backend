7c6d5d096b0018184377b35b1c036e61b6529ea1
Grofes <92209055+Grofes@users.noreply.github.com>
2023-05-23 17:07:20 +0300

    Update index.php

--- num5/index.php
+++ num5/index.php
@@ -88,6 +88,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $db = new PDO('mysql:host=localhost;dbname=u52821', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
     try{
       $get=$db->prepare("select * from form where id=?");
+      print($_SESSION['uid']);
       $get->bindParam(1,$_SESSION['uid']);
       $get->execute();
       $inf=$get->fetchALL();
